e24289cad88ab5a6de916bc2d5015b2ba94bc1e7
Bharat Bhushan <bharatsoni1996@gmail.com>
2020-05-20 19:15:31 +0530

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material';
+import { MatProgressSpinnerModule,
+         MatRadioModule,
+         MatSliderModule,
+         MatCardModule,
+         MatAutocompleteModule,
+         MatCheckboxModule,
+         MatSlideToggleModule,
+         MatSelectModule,
+         MatProgressBarModule,
+         MatMenuModule,
+         MatGridListModule,
+         MatTableModule,
+         MatPaginatorModule,
+         MatSnackBarModule
+
+
+ } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -9,7 +40,35 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+
+    MatTableModule,
+    MatPaginatorModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatTabsModule,
+    MatRadioModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatListModule,
+    MatStepperModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSliderModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatProgressBarModule,
+    MatGridListModule,    
   ],
   providers: [],
   bootstrap: [AppComponent]
